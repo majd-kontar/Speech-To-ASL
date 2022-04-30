bbc94a27d8d978b91ed49c6db053855660227309
Majd Kontar <Majdkontar@gmail.com>
2022-04-30 19:23:37 +0300

    Add: MATLAB Simulink model

---
+++ MATLAB/Params.m
@@ -0,0 +1,69 @@
+    spacing = 5;
+    fingers_spacing = 30;
+
+% Arm
+    arm_radius = 45;
+    arm_length = 100;
+
+% Palm
+    palm_height = 76;
+    palm_width = 115;
+    palm_thickness = 25;
+
+% Index Finger
+%   First Bone
+    index_radius1 = 10;
+    index_length1 = 30; 
+%   Second Bone
+    index_radius2 = 10;
+    index_length2 = 30;
+%   Third Bone
+    index_radius3 = 10;
+    index_length3 = 30;
+
+%   Middle Finger
+%   First Bone
+    middle_radius1 = 11;
+    middle_length1 = 30; 
+%   Second Bone
+    middle_radius2 = 11;
+    middle_length2 = 30;
+%   Third Bone
+    middle_radius3 = 11;
+    middle_length3 = 30;
+
+%   Ring Finger
+%   First Bone
+    ring_radius1 = 11;
+    ring_length1 = 30; 
+%   Second Bone
+    ring_radius2 = 11;
+    ring_length2 = 30;
+%   Third Bone
+    ring_radius3 = 11;
+    ring_length3 = 30;
+
+%   Little Finger
+%   First Bone
+    little_radius1 = 10;
+    little_length1 = 30; 
+%   Second Bone
+    little_radius2 = 10;
+    little_length2 = 30;
+%   Third Bone
+    little_radius3 = 10;
+    little_length3 = 30;
+
+%   Thumb
+    thumb_offset_x = 5;
+    thumb_offset_y = 30;
+    thumb_angle = 30;
+%   First Bone
+    thumb_radius1 = 10;
+    thumb_length1 = 10; 
+%   Second Bone
+    thumb_radius2 = 10;
+    thumb_length2 = 20;
+%   Thiird Bone
+    thumb_radius3 = 10;
+    thumb_length3 = 20;
